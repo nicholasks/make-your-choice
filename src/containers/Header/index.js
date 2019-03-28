c56ef8f00177582ab75dd930a25ec6952ef5d4c3
Nicholas Kegler <nicholasks@gmail.com>
2019-03-28 10:15:46 -0300

    Basic Title with login button and more

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import './index.css';
 
-export default class Header extends Component {
-
+class Header extends Component {
   render() {
     return(
-      <div>
-        <h1>Titulo Maroto</h1>
+      <div style={divStyle}>
+        <header style={headerStyle}>
+          <h1 style={{flex: 11}}>Make your Choice</h1>
+          <p style={{flex: 1, padding: '15px'}}>Login</p>
+        </header>
+        <div style={{display: 'flex', direction: 'row'}}>
+          <p style={pStyle}>Live Status</p>
+          <p style={pStyle}>Vote now</p>
+        </div>
       </div>
-    );
+    )
   }
-
 }
+
+const divStyle = {
+  border: '2px solid blue',
+  margin: '0px'
+}
+
+const headerStyle = {
+  display: 'flex',
+  background: '#282c34',
+  color: '#fff',
+  padding: '10px',
+  margin: '0px',
+  fontFamily: 'Courier New'
+}
+
+const pStyle = {
+  flex: '1',
+  border: '1px solid red'
+}
+
+export default Header;
