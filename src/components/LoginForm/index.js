f2616b5a8c38b10b7c1ac07859f1f43ba7c22035
Nicholas Kegler <nicholasks@gmail.com>
2019-03-28 15:17:19 -0300

    Ugly login form created

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -5,15 +5,38 @@ class LoginForm extends Component {
   render() {
     return(
       <React.Fragment>
-        <h1>Login</h1>
-        <form>
-          
+        <h1><center>Login Ugly Form here</center></h1>
+        <form className="formzinho" style={formStyle}>
+          <div style={divStyle}>
+            Name: <input type="textField"></input>
+          </div>
+          <div style={divStyle}>
+            Username: <input type="textField"></input>
+          </div>
+          <div style={divStyle}>
+            Password: <input type="password"></input>
+          </div>
+          <div style={divStyle}>
+            Password confirmation:  <input type="password"></input>
+          </div>
+          <div style={divStyle}>
+            <input type="button" value="Go or type ENTER"></input>
+          </div>
         </form>
       </React.Fragment>
     )
   }
 }
 
+const divStyle = {
+  margin: '10px 10%'
+}
+
+const formStyle = {
+  display: 'flex',
+  flexDirection: 'column'
+
+}
 const h1Style = {
   textAlign: 'center',
   color: 'grey'
