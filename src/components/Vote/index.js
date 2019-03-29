735da9bc9bb679760ca799079c21798bdc3bbe17
Nicholas Kegler <nicholasks@gmail.com>
2019-03-29 16:09:45 -0300

    Wrapped on the class.

--- src/components/Vote/index.js
+++ src/components/Vote/index.js
@@ -1,80 +1,74 @@
 import React, { Component } from 'react';
 import ReactTable from "react-table";
+import update from 'react-addons-update';
 import 'react-table/react-table.css'
 
+import styles from './styles';
+
 class Vote extends Component {
+  state = {
+    columns: [
+      {
+        Header: 'ID',
+        accessor: 'id' // String-based value accessors!
+      },
+      {
+        Header: 'Character name',
+        accessor: 'character' // String-based value accessors!
+      },
+      {
+        Header: 'Select',
+        accessor: 'selected',
+      }
+    ],
+
+    data: [
+      {
+        id: '0',
+        character: 'GUlana'
+      },
+      {
+        id: '1',
+        character: 'Cicrana'
+      },
+      {
+        id: '2',
+        character: 'Beltrana'
+      },
+      {
+        id: '3',
+        character: 'Beltrana22'
+      },
+      {
+        id: '4',
+        character: 'Suzana'
+      }
+    ]
+  }
+
+  addOrRm = (ev) => {
+    console.log(ev.target.id);
+  }
+
+  addCheckBox = () => {
+    for (var i in this.state.data) {
+      var botao = <input id={this.state.data[i].id} type='checkbox' onChange={(ev) => {this.addOrRm(ev)}} style={styles.inpStyle}></input>;
+      this.state.data[i].selected = botao;
+    }
+  }
+
   render() {
     return(
       <React.Fragment>
-        <h1 style={h1Style}>Vote Now</h1>
-        <ReactTable className='-striped' sortable={false} style={tableStyle} data={data} columns={columns} pageSize={'18'}/>
+        {this.addCheckBox()}
+        <h1 style={styles.h1Style}>Vote Now</h1>
+        <ReactTable className='-striped' sortable={false} style={styles.tableStyle} data={this.state.data} columns={this.state.columns} pageSize={'10'}/>
       </React.Fragment>
     )
   }
 }
 
-var selected = []
-
-const tableStyle = {
-  textAlign: 'center',
-  textOverflow: 'ellipsis',
-}
-
-const h1Style = {
-  textAlign: 'center',
-  color: 'grey'
-}
-
 var d = new Date();
 var dattt = d.toISOString();
-
-const data = [
-  {
-    id: '0',
-    character: 'GUlana',
-    selected: <input type='checkbox' style={inpStyle}></input>
-  },
-  {
-    id: '1',
-    character: 'Cicrana',
-    selected: <input type='checkbox' style={inpStyle}></input>
-  },
-  {
-    id: '2',
-    character: 'Beltrana',
-    selected: <input type='checkbox' style={inpStyle}></input>
-  },
-  {
-    id: '3',
-    character: 'Beltrana22',
-    selected: <input type='checkbox' style={inpStyle}></input>
-  },
-  {
-    id: '4',
-    character: 'Suzana',
-    selected: <input type='checkbox' onChange={(ev) => {console.log(ev);}}style={inpStyle}></input>
-  }
-]
-
-const columns = [
-  {
-    Header: 'ID',
-    accessor: 'id' // String-based value accessors!
-  },
-  {
-    Header: 'Character name',
-    accessor: 'character' // String-based value accessors!
-  },
-  {
-    Header: 'Select',
-    accessor: 'selected',
-  }
-]
-
-const inpStyle = {
-  margin: '25px',
-  border: '10px',
-  type: 'checkbox'
-}
 
 export default Vote;
---
+++ src/components/Vote/styles.js
@@ -0,0 +1,17 @@
+const styles = {
+  tableStyle: {
+    textAlign: 'center',
+    textOverflow: 'ellipsis', 
+  },
+  h1Style :{
+    textAlign: 'center',
+    color: 'grey'
+  },
+  inpStyle:{
+   margin: '25px',
+   border: '10px',
+   type: 'checkbox'
+ }
+}
+
+export default styles;
